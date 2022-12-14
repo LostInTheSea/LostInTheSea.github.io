f0003642b0d41ad715474fe287a9e8380ca93521
LostInTheSea <116543954+LostInTheSea@users.noreply.github.com>
2022-12-14 13:16:54 +0100

    Add files via upload

---
+++ login/js/mittscript.js
@@ -0,0 +1,62 @@
+/* Kommentarer i 
+   JavaScript kan 
+   gå over flere linjer	   
+*/
+	
+// eller bare en enkelt linje 
+
+
+// Nå skal vi definere en funksjon som henter variabler fra HTML-form
+
+function variablerFraForm(){
+	
+	// det er god praksis å deklarere lokale variabler (variabler som benyttes inne i en funksjon) i starten av funksjonen
+	
+	let variabel_brukernavn ="";
+	let variabel_passord ="";
+	let korrekt_brukernavn ="TheGaiaProject";
+	let korrekt_passord ="pz";
+	let myOutput = "";
+	let myDebug = 0;
+	// let returVerdi = "";
+
+	// tilordne lokale variabler med verdier fra skjema/form
+	// vi bruker document.getElementById(id/variabelnavn_i_form)
+
+	variabel_brukernavn = document.getElementById("brukernavn").value;
+	variabel_passord = document.getElementById("passord").value;
+
+	
+	// console.log brukes for utvikling/debugging
+	// output til console.log i browser inspector (ctrl/cmd+i)
+	// vis variablene i konsoll-loggen
+	
+	if(myDebug == 1){
+		console.log("Brukernavn : " + variabel_brukernavn);
+		console.log("Passord : " + variabel_passord);
+	}
+	
+	
+	//sjekk bruker navn
+	if(variabel_brukernavn == korrekt_brukernavn){
+		myOutput = myOutput + "Feil Bruker navn eller Passord er feil <br> prøv på nytt";
+
+		//sjekk bruker passord
+		if(variabel_passord == korrekt_passord){
+			myOutput = " welcommen";
+		
+		}
+	}
+	else{
+		myOutput = myOutput + "Feil Bruker navn eller Passord er feil <br> prøv på nytt";
+
+	}
+	// legge til det som skal vises i html-filen i variabelen myOutput
+	//
+
+	// myOutput = "Hei " + variabel_brukernavn + " !";
+	
+	// vise output i en spesifikk paragraph <p id="output"> i html-filen	
+	document.getElementById("output").innerHTML = myOutput;	
+	
+}
